596b79edfe85e544c768f0017abc9be1bbf0e0e9
yunlin.luo <yunlin.luo@grei.com.au>
2017-05-25 16:27:16 +0800

    MSSysShare

--- MusouKit/MusouKit/Utils/MSSysShare.h
+++ MusouKit/MusouKit/Utils/MSSysShare.h
@@ -9,7 +9,21 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
+/**
+ * 调用系统分享组件进行分享，无须导入第三方组件.
+ * 注意：
+ * 1.Weibo的icon必须要在设置中登录了微博账号才会出现
+ * 2.Facebook、Twitter等同上
+ * 3.微信分享必须要带有link参数，否则会提示不支持的类型
+ */
 @interface MSSysShare : NSObject
+/** Will be called before share */
+@property (nonatomic, copy) void (^beforeShare)(void);
+/** Excluded activity types */
+@property (nonatomic, strong) NSArray<UIActivityType> *excludedTypes;
+
+/** Singleton */
++ (instancetype)shared;
 
 /**
  * Share with text, image and link
@@ -22,5 +36,11 @@
         image:(UIImage *)image
          link:(NSURL *)link
    completion:(void (^)(BOOL completed, NSError *error))completion;
+
+/**
+ * Check if the service is logined
+ * @param serviceType e.g. SLServiceTypeSinaWeibo, see SLServiceTypes
+ */
++ (BOOL)isServiceAvailable:(NSString *)serviceType;
 
 @end
--- MusouKit/MusouKit/Utils/MSSysShare.m
+++ MusouKit/MusouKit/Utils/MSSysShare.m
@@ -7,33 +7,131 @@
 //
 
 #import "MSSysShare.h"
+#import <Social/Social.h>
+
+@interface MSWeiboActivity : UIActivity
+@property (nonatomic, strong) NSArray *activityItems;
+@end
+
+@implementation MSWeiboActivity
+
+- (NSString *)activityType{
+    return UIActivityTypePostToWeibo;
+}
+
+- (NSString *)activityTitle{
+    return @"Weibo";
+}
+
+- (UIImage *)activityImage{
+    return nil;
+}
+
+- (BOOL)canPerformWithActivityItems:(NSArray *)activityItems{
+    return YES;
+}
+
+- (void)performActivity{
+    //Call social kit
+    SLComposeViewController *compose = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeSinaWeibo];
+    
+    for (id item in self.activityItems){      
+        if ([item isKindOfClass:[NSString class]]){
+            [compose setInitialText:item];
+        } else if ([item isKindOfClass:[UIImage class]]){
+            [compose addImage:item];
+        } else if ([item isKindOfClass:[NSURL class]]){
+            [compose addURL:item];
+        }
+    }
+
+    [compose setCompletionHandler:^(SLComposeViewControllerResult result){
+        [self activityDidFinish:result == SLComposeViewControllerResultDone];
+    }];
+    UIViewController *controller = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    [controller presentViewController:compose animated:YES completion:nil];
+    
+}
+
+@end
+
+
 
 @implementation MSSysShare
 
++ (instancetype)shared{
+    static dispatch_once_t onceToken;
+    static MSSysShare *_sysShare;
+    dispatch_once(&onceToken, ^{
+        _sysShare = [[self alloc] init];
+    });
+    return _sysShare;
+}
+
+- (id)init{
+    self = [super init];
+    if (self){
+        self.excludedTypes = @[
+                               //UIActivityTypeMail,
+                               UIActivityTypeAssignToContact,
+                               UIActivityTypeSaveToCameraRoll,
+                               UIActivityTypeAddToReadingList,
+                               UIActivityTypeAirDrop,
+                               UIActivityTypeOpenInIBooks,
+                               ];
+        
+        self.beforeShare = ^{
+            static dispatch_once_t onceToken;
+            dispatch_once(&onceToken, ^{
+                
+                if (![MSSysShare isServiceAvailable:SLServiceTypeSinaWeibo]){
+                    [MSSysShare alertTips:NSLocalizedString(@"如要分享到微博，请先到系统设置中登录您的微博账号", nil)];
+                }
+            });
+        };
+    }
+    return self;
+}
+
 - (void)share:(NSString *)text image:(UIImage *)image link:(NSURL *)link completion:(void (^)(BOOL completed, NSError *error))completion{
+    
+    if (self.beforeShare){
+        self.beforeShare();
+        self.beforeShare = nil;
+    }
     
     NSMutableArray *items = [NSMutableArray new];
     if (text) [items addObject:text];
     if (image) [items addObject:image];
     if (link) [items addObject:link];
     
-    UIActivityViewController *act = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:nil];
-    act.excludedActivityTypes = @[
-                                  //UIActivityTypeMail,
-                                  UIActivityTypePrint,
-                                  UIActivityTypeOpenInIBooks,
-                                  UIActivityTypeAssignToContact,
-                                  UIActivityTypeAddToReadingList
-                                  ];
+    //Weibo icon will auto appear when you login weibo, which in the system settings
+    //MSWeiboActivity *wb = [MSWeiboActivity new];
+    
+    UIActivityViewController *act = [[UIActivityViewController alloc] initWithActivityItems:items
+                                                                      applicationActivities:nil];
+    act.excludedActivityTypes = self.excludedTypes;
     [act setCompletionWithItemsHandler:^(UIActivityType activityType, BOOL completed, NSArray *returnedItems, NSError *activityError){
-        NSLog(@"completed:%d,%@,%@", completed, returnedItems, activityError);
+        
         if (completion) completion(completed, activityError);
     }];
     
     UIViewController *controller = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-    [controller presentViewController:act animated:YES completion:^{
-        
-    }];
+    [controller presentViewController:act animated:YES completion:nil];
+}
+
++ (BOOL)isServiceAvailable:(NSString *)serviceType{
+    return [SLComposeViewController isAvailableForServiceType:serviceType];
+}
+
++ (void)alertTips:(NSString *)tips{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil
+                                                    message:tips
+                                                   delegate:nil
+                                          cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                                          otherButtonTitles:nil];
+    [alert show];
 }
 
 @end
+
